367bbbb89eaee86f9a030b844de1e7eb30668304
Valerio Mattera <matteravalerio@gmail.com>
2024-12-02 17:49:58 +0000

    Day 2 challenges

---
+++ day-2/challenge-1.ts
@@ -0,0 +1,53 @@
+import { challengeInput, exampleInput } from './data'
+
+type Report = number[]
+
+const getReports = (input: string) => {
+  const reports = input.split('\n').reduce((acc: Report[], curr) => {
+    if (!curr) return acc
+    const reportWithNumbers = curr.split(' ').map((n) => +n)
+    acc.push(reportWithNumbers)
+    return acc
+  }, [])
+
+  return reports
+}
+
+const isAlwaysIncreasingOrDecreasing = (report: Report) => {
+  const originalString = report.join('')
+  const sortedString = report.sort((a, b) => a - b).join('')
+  const reverseString = report
+    .sort((a, b) => b + a)
+    .reverse()
+    .join('')
+  return originalString === sortedString || originalString === reverseString
+}
+
+const isElementDifferenceBetweenOneAndThree = (report: Report) => {
+  for (let index = 0; index < report.length - 1; index++) {
+    const current = report[index]
+    const next = report[index + 1]
+    const difference = Math.abs(next - current)
+    if (difference === 0 || difference > 3) return false
+  }
+  return true
+}
+
+const getSafeReports = (input: string) => {
+  const reports = getReports(input)
+
+  let safeQuantity = 0
+  reports.forEach((report) => {
+    if (!isAlwaysIncreasingOrDecreasing(report)) return
+    if (isElementDifferenceBetweenOneAndThree(report)) {
+      safeQuantity++
+    }
+  })
+  return safeQuantity
+}
+
+const exampleResult = getSafeReports(exampleInput)
+console.log(exampleResult)
+
+const challengeResult = getSafeReports(challengeInput)
+console.log(challengeResult)
---
+++ day-2/challenge-2.ts
@@ -0,0 +1,92 @@
+import { challengeInput, exampleInput } from './data'
+
+type Report = number[]
+type Direction = 'increasing' | 'decreasing'
+
+class SafeReports {
+  private reports: Report[]
+  safeReportsQuantity: number
+  constructor(private input: string) {
+    this.reports = this.getReports()
+    this.safeReportsQuantity = this.getSafeReports()
+  }
+
+  private getReports = () => {
+    const reports = this.input.split('\n').reduce((acc: Report[], curr) => {
+      if (!curr) return acc
+      const reportWithNumbers = curr.split(' ').map((n) => +n)
+      acc.push(reportWithNumbers)
+      return acc
+    }, [])
+    return reports
+  }
+
+  private getDifference = (current: number, next: number, direction: Direction) =>
+    direction === 'increasing' ? next - current : current - next
+
+  private isDifferenceOK = (difference: number) => difference > 0 && difference <= 3
+
+  private findIncreasingOrDecreasing = (report: Report): Direction => {
+    let sumOfDifferences = 0
+    report.forEach((curr, index) => {
+      const next = report[index + 1]
+      if (next) {
+        next - curr > 0 ? sumOfDifferences++ : sumOfDifferences--
+      }
+    })
+    return sumOfDifferences > 0 ? 'increasing' : 'decreasing'
+  }
+
+  private isValidReport = (report: Report) => {
+    const direction = this.findIncreasingOrDecreasing(report)
+
+    let remainingSkips = 1
+    let index = 0
+    let current = report[index]
+    let next = report[index + 1]
+
+    while (next !== undefined) {
+      const difference = this.getDifference(current, next, direction)
+
+      if (this.isDifferenceOK(difference)) {
+        index++
+      } else {
+        if (!remainingSkips) return false
+        const nextNext = report[index + 2]
+        const differenceCurrentAndNextNext = this.getDifference(current, nextNext, direction)
+        const differenceNextAndNextNext = this.getDifference(next, nextNext, direction)
+
+        // It means only the last level is wrong
+        if (isNaN(differenceCurrentAndNextNext) && isNaN(differenceNextAndNextNext)) {
+          return true
+        }
+
+        if (this.isDifferenceOK(differenceCurrentAndNextNext)) {
+          report.splice(index + 1, 1)
+        } else if (index === 0 && this.isDifferenceOK(differenceNextAndNextNext)) {
+          report.splice(0, 1)
+        } else {
+          return false
+        }
+        remainingSkips--
+      }
+      current = report[index]
+      next = report[index + 1]
+    }
+    return true
+  }
+
+  private getSafeReports = () => {
+    let safeQuantity = 0
+    this.reports.forEach((report) => {
+      this.isValidReport(report) && safeQuantity++
+    })
+    return safeQuantity
+  }
+}
+
+const { safeReportsQuantity: exampleResult } = new SafeReports(exampleInput)
+console.log(exampleResult)
+
+const { safeReportsQuantity: challengeResult } = new SafeReports(challengeInput)
+console.log(challengeResult)
